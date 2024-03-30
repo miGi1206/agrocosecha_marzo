7b910ea2999f7abe8490a5eca0f0b8caa7d3c4f2
Braider <braider200329@gmail.com>
2024-03-30 18:17:28 -0500

    Arreglé carrito

--- view/inc/navbar_menu.php
+++ view/inc/navbar_menu.php
@@ -27,15 +27,14 @@
             }
         ?>
 
-        <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between"
-            id="templatemo_main_nav">
+        <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-end" id="templatemo_main_nav">
             <div class="flex-fill">
                 <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo SERVERURL;?>home-agro/">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="<?php echo SERVERURL;?>productos-servicios?busqueda=<?php echo $primer_id; ?>">Productos y Servicios</a>
+                        <a class="nav-link" href="<?php echo SERVERURL;?>productos-servicios?busqueda=<?php echo $primer_id; ?>">Productos y Servicios</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo SERVERURL;?>quienes-somos/">Quienes somos</a>
@@ -43,57 +42,48 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo SERVERURL;?>contactanos/">Contactanos</a>
                     </li>
-                    <?php
-                        if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "2"){ ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?php echo SERVERURL;?>checkout/">Carrito(
-                                    <?php echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']);?>)    
-                                </a>
-                            </li>
-                    <?php } ?>
                 </ul>
             </div>
 
-            <!-- //TODO: Funcion para mostrar la opcion de iniciar sesion -->
-            <?php if (!isset($_SESSION['codigo_usuario_spm'])) { ?>
-            <div class="navbar align-self-center d-flex">
+            <!-- Elemento del carrito -->
+            <?php if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "2"): ?>
                 <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
                     <li class="nav-item">
-                    <a class="nav-link" href="<?php echo SERVERURL; ?>login/">iniciar sesión</a>
+                        <a class="nav-link" href="<?php echo SERVERURL;?>checkout/">Carrito (<?php echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']);?>)</a>
                     </li>
                 </ul>
-            </div>
-            <?php
+            <?php endif; ?>
 
-            //TODO: Funcion si no se cumple la anterior; Muestra un icono de usuario, si es administrador 
-            //todo: muestra una opcion para ir a las tablas de los clientes, proveedores y otros administradores
-            } else { 
-                //* Asegúrate de definir $nombre aunque sea como una cadena vacía
-                $nombre = isset($_SESSION['usuario_spm']) ? $_SESSION['usuario_spm'] : '';
-            ?>
-                <div class="navbar align-self-center d-flex">
-                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="usuarioDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <?php
-                                if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "3") { ?>
-                                    <i class="bi bi-person text-dark"></i> <?php echo $_SESSION['nombre_razonsocial_spm']; ?>
-                                <?php }else { ?>
-                                    <i class="bi bi-person text-dark"></i> <?php echo $_SESSION['primer_nombre_spm']; ?>
-                                <?php } ?>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu dropdown-menu-end" aria-labelledby="usuarioDropdown">
-                                <?php
-                                if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "1") { ?>
-                                    <a class="dropdown-item" href="<?php echo SERVERURL;?>personas-list/">Admin</a>
-                                <?php } ?>
-                                <a href="" class="btn-exit-system2 dropdown-item"><i class="lni lni-exit"></i>Cerrar sesion</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            <?php }?>
+            <!-- Mostrar la opción de iniciar sesión o el icono de usuario -->
+            <?php if (!isset($_SESSION['codigo_usuario_spm'])): ?>
+                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo SERVERURL; ?>login/">Iniciar sesión</a>
+                    </li>
+                </ul>
+            <?php else: ?>
+                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="usuarioDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <?php if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "3"): ?>
+                                <i class="bi bi-person text-dark"></i> <?php echo $_SESSION['nombre_razonsocial_spm']; ?>
+                            <?php else: ?>
+                                <i class="bi bi-person text-dark"></i> <?php echo $_SESSION['primer_nombre_spm']; ?>
+                            <?php endif; ?>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu dropdown-menu-end" aria-labelledby="usuarioDropdown">
+                            <?php if (isset($_SESSION['tipo_usuario_spm']) && $_SESSION['tipo_usuario_spm'] == "1"): ?>
+                                <a class="dropdown-item" href="<?php echo SERVERURL;?>personas-list/">Admin</a>
+                            <?php endif; ?>
+                            <a href="" class="btn-exit-system2 dropdown-item"><i class="lni lni-exit"></i>Cerrar sesion</a>
+                        </div>
+                    </li>
+                </ul>
+            <?php endif; ?>
+
+            
         </div>
+
     </div>
 </nav>
 <?php include "./view/inc/LogOut.php"; ?>
