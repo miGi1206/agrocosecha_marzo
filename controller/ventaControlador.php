30c78f4ec38b9348cc0467b18266e2dd4bf2814f
juan jose <denyamanx133@gmail.com>
2024-04-01 00:48:43 -0500

    arreglamos tabla ventaControlador

--- controller/ventaControlador.php
+++ controller/ventaControlador.php
@@ -4,45 +4,40 @@ class ventaControlador extends mainModel
     /*--------------------- TABLA PAGINADOR ---------------------------*/
     public function paginador_venta_controlador($pagina, $registros, $id, $url, $busqueda)
     {
-            $pagina = mainModel::limpiar_cadena($pagina);
-            $registros = mainModel::limpiar_cadena($registros);
-            $id = mainModel::limpiar_cadena($id);
-            $url = mainModel::limpiar_cadena($url);
-            $url = SERVERURL . $url . "/";
-            $busqueda = mainModel::limpiar_cadena($busqueda);
-            $tabla = "";
-            
-            $pagina = (isset($pagina) && $pagina > 0) ? (int) $pagina : 1;
-            $inicio = ($pagina > 0) ? (($pagina * $registros) - $registros) : 0;
-            
-            if (isset($busqueda) && $busqueda != "") {
-                $consulta = "SELECT SQL_CALC_FOUND_ROWS tbl_venta.ticket, tbl_venta.fecha_venta, tbl_venta.direccion, tbl_venta.sub_total, tbl_venta.total_venta, tbl_venta.iva, tbl_persona.identificacion, tbl_persona.primer_nombre 
-                            FROM tbl_venta 
-                            INNER JOIN tbl_persona ON tbl_venta.cod_persona = tbl_persona.identificacion 
-                            WHERE tbl_persona.identificacion LIKE '%$busqueda%' 
-                            ORDER BY tbl_venta.ticket ASC LIMIT $inicio, $registros";
-            } else {
-                $consulta = "SELECT SQL_CALC_FOUND_ROWS tbl_venta.ticket, tbl_venta.fecha_venta, tbl_venta.direccion, tbl_venta.sub_total, tbl_venta.total_venta, tbl_venta.iva, tbl_persona.identificacion, tbl_persona.primer_nombre 
-                FROM tbl_venta 
-                INNER JOIN tbl_persona ON tbl_venta.cod_persona = tbl_persona.codigo_persona 
-                ORDER BY tbl_venta.ticket;
-                ASC LIMIT $inicio, $registros";
-            }
-            
-            
-            
-            
-            
-            $conexion = mainModel::conectar();
-            $datos = $conexion->query($consulta);
-            $datos = $datos->fetchAll();
-    
-            $total = $conexion->query("SELECT FOUND_ROWS()");
-            $total = (int) $total->fetchColumn();
-            
-            $Npaginas = ceil($total / $registros);
-    
-            $tabla .= '<div class="">
+        $pagina = mainModel::limpiar_cadena($pagina);
+        $registros = mainModel::limpiar_cadena($registros);
+        $id = mainModel::limpiar_cadena($id);
+        $url = mainModel::limpiar_cadena($url);
+        $url = SERVERURL . $url . "/";
+        $busqueda = mainModel::limpiar_cadena($busqueda);
+        $tabla = "";
+
+        $pagina = (isset($pagina) && $pagina > 0) ? (int)$pagina : 1;
+        $inicio = ($pagina > 0) ? (($pagina * $registros) - $registros) : 0;
+
+        if (isset($busqueda) && $busqueda != "") {
+            $consulta = "SELECT SQL_CALC_FOUND_ROWS tbl_venta.ticket, tbl_venta.fecha_venta, tbl_venta.direccion, tbl_venta.sub_total, tbl_venta.total_venta, tbl_venta.iva, tbl_persona.identificacion, tbl_persona.primer_nombre 
+                        FROM tbl_venta 
+                        INNER JOIN tbl_persona ON tbl_venta.cod_persona = tbl_persona.identificacion 
+                        WHERE tbl_persona.identificacion LIKE '%$busqueda%' 
+                        ORDER BY tbl_venta.ticket ASC LIMIT $inicio, $registros";
+        } else {
+            $consulta = "SELECT SQL_CALC_FOUND_ROWS tbl_venta.ticket, tbl_venta.fecha_venta, tbl_venta.direccion, tbl_venta.sub_total, tbl_venta.total_venta, tbl_venta.iva, tbl_persona.identificacion, tbl_persona.primer_nombre 
+            FROM tbl_venta 
+            INNER JOIN tbl_persona ON tbl_venta.cod_persona = tbl_persona.codigo_persona 
+            ORDER BY tbl_venta.ticket ASC LIMIT $inicio, $registros";
+        }
+
+        $conexion = mainModel::conectar();
+        $datos = $conexion->query($consulta);
+        $datos = $datos->fetchAll();
+
+        $total = $conexion->query("SELECT FOUND_ROWS()");
+        $total = (int)$total->fetchColumn();
+
+        $Npaginas = ceil($total / $registros);
+
+        $tabla .= '<div class="">
             <table class="table table-hover table-sm">
             <tr>
             <th class="text-center">N°</th>
@@ -56,23 +51,23 @@ class ventaControlador extends mainModel
             <th class="text-center">Nombre</th>
             </tr>
             ';
+
         if ($total >= 1 &&  $pagina <= $Npaginas) {
             $contador = $inicio + 1;
             $reg_inicio = $inicio + 1;
             foreach ($datos as $rows) {
-                    $tabla .=
-                        '<tr class="p">
-                        <td class="min-width">' . $contador . '</td>
-                        <td class="min-width">' . $rows['ticket'] . '</td>
-                        <td class="min-width">' . $rows['fecha_venta'] .'</td>
-                        <td class="min-width">' . $rows['direccion'] .' </td>
-                        <td class="min-width">' . $rows['sub_total'] . '</td> 
-                        <td class="min-width">' . $rows['total_venta'] . '</td>
-                        <td class="min-width">' . $rows['iva'] . '</td>   
-                        <td class="min-width">' . $rows['identificacion'] . '</td>   
-                        <td class="min-width">' . $rows['primer_nombre'] . '</td>   
-                    </tr>';
-                
+                $tabla .=
+                    '<tr class="p">
+                    <td class="min-width">' . $contador . '</td>
+                    <td class="min-width">' . $rows['ticket'] . '</td>
+                    <td class="min-width">' . $rows['fecha_venta'] .'</td>
+                    <td class="min-width">' . $rows['direccion'] .' </td>
+                    <td class="min-width">' . $rows['sub_total'] . '</td> 
+                    <td class="min-width">' . $rows['total_venta'] . '</td>
+                    <td class="min-width">' . $rows['iva'] . '</td>   
+                    <td class="min-width">' . $rows['identificacion'] . '</td>   
+                    <td class="min-width">' . $rows['primer_nombre'] . '</td>   
+                </tr>';
                 $contador++;
             }
             $reg_final = $contador - 1;
